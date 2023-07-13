51350466288a291a9a0facba9c2688283227b136
Guyven <guyven@pm.me>
2023-07-13 17:27:50 -0400

    some test output from parser showing

--- src/editor.ts
+++ src/editor.ts
@@ -1,17 +1,22 @@
 import m from "mithril"
-//import { test, test2 } from "./parser-textmate";
-import { test } from "./parser"
+import { sqlite, display } from "./parser"
 
 const editorClass = ` 
 
 `
 
-export type EditorState = Partial<{ text: string }>
+export type EditorState = Partial<{ text: string; parser: any }>
 export type EditorAttrs = { stateContainer: EditorState }
 
 export const Editor: m.ClosureComponent<EditorAttrs> = initialProps => {
   const { stateContainer: state } = initialProps.attrs
-  test()
+  const s = sqlite({ initialText: state.text, onReady: m.redraw })
+
+  // state.parser = null
+  // const updateText = (text: string) => {
+  //   state.parser = s.updateText(text)
+  // }
+
   return {
     view: () => {
       return m(
@@ -20,8 +25,13 @@ export const Editor: m.ClosureComponent<EditorAttrs> = initialProps => {
         m("textarea", {
           value: state.text,
           placeholder: "Type here...",
-          oninput: (e: any) => (state.text = e.target.value),
-        })
+          oninput: (e: any) => (
+            (state.text = e.target.value), s.updateText(e.target.value)
+          ),
+        }),
+        m("pre", state.text),
+        m("pre", s.tree?.rootNode.toString()),
+        m("pre", display(s.tree))
       )
     },
   }
--- src/parser.ts
+++ src/parser.ts
@@ -1,6 +1,6 @@
 import TreeSitter from "web-tree-sitter"
 
-const sample =`
+const sample = `
 SELECT *
 FROM abc
 WHERE abc.xyz = 'def'
@@ -15,9 +15,43 @@ export const test = async () => {
   //   console.log("tree sitter loaded")
   // })
   await TreeSitter.init()
-  const parser = new TreeSitter
-  const sqlLang = await TreeSitter.Language.load('tree-sitter-sqlite.wasm')
+  const parser = new TreeSitter()
+  const sqlLang = await TreeSitter.Language.load("tree-sitter-sqlite.wasm")
   parser.setLanguage(sqlLang)
   const tree = parser.parse(sample)
   console.log(tree.rootNode.toString())
+}
+
+export const display = (
+  tree: TreeSitter.Tree | undefined,
+  root: TreeSitter.SyntaxNode | undefined = undefined
+): string => {
+  const r = root || tree?.rootNode
+  if (!r) return "<empty tree>"
+  const children = r.children.map(c => display(tree, c))
+
+  return children.length ? children.join(" ") : r.text
+}
+
+export const sqlite = ({ initialText = sample, onReady = () => {} } = {}) => {
+  let parser: TreeSitter | undefined = undefined
+  let tree: TreeSitter.Tree | undefined = undefined
+  const init = async () => {
+    await TreeSitter.init()
+    parser = new TreeSitter()
+    const sqlLang = await TreeSitter.Language.load("tree-sitter-sqlite.wasm")
+    parser.setLanguage(sqlLang)
+    tree = parser.parse(initialText)
+  }
+
+  init().then(onReady)
+
+  return {
+    updateText: (text: string) => {
+      tree = parser?.parse(text)
+    },
+    get tree() {
+      return tree
+    },
+  }
 }
